eb8c86b72dbfbb0426a97ced0dedcb4b668c9860
Erin <qwey1584@gmail.com>
2024-04-16 20:51:47 +0900

    게시글 동적 구현

---
+++ StackGrowth/Assets/Scripts/spawner.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class spawner : MonoBehaviour
+{
+    public GameObject post;
+    public Transform spawn_point;
+    int spacing = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            spawn();
+        }
+    }
+
+    public void spawn()
+    {
+        RectTransform lastPostTransform = (spawn_point.childCount > 0) ? spawn_point.GetChild(spawn_point.childCount - 1).GetComponent<RectTransform>() : null;
+        GameObject instance = Instantiate(post, spawn_point);
+        RectTransform newPostTransform = instance.GetComponent<RectTransform>();
+        if (lastPostTransform != null)
+        {
+            float posY = lastPostTransform.localPosition.y - lastPostTransform.rect.height - spacing;
+            newPostTransform.localPosition = new Vector3(0f, posY, 0f);
+        }
+        else
+            newPostTransform.localPosition = Vector3.zero;
+
+        newPostTransform.SetParent(spawn_point, false);
+    }
+}
